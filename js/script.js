fef30a41e5118fe61eb80f81ca7b44c533fa5826
Will Westwood <wwstwd@gmail.com>
2021-01-25 12:53:41 +0000

    Final I hope

--- js/script.js
+++ js/script.js
@@ -22,12 +22,10 @@ function createScene() {
     var gl = new BABYLON.GlowLayer("glow", scene);
     gl.intensity = 1.5;
 
-    // BABYLON.SceneLoader.ImportMesh("", "./models/", "Fan.glb", scene, function(scene) {
-    //     document.getElementById("fadeOutContainer").style.opacity = 0;
-    //     setTimeout(function() {
-    //         // document.getElementById("fadeOutContainer").remove();
-    //     }, 1000)
-    // })
+    BABYLON.SceneLoader.ImportMesh("", "./models/", "Gen.glb", scene, function(scene) {
+        setTimeout(function() {
+        }, 1000)
+    })
 
     if (window.matchMedia("(max-width: 700px)").matches) {
         camera.detachControl()
@@ -47,58 +45,48 @@ window.addEventListener("resize", function () {
     engine.resize();
 });
 
-function changeMesh(meshName) {
 
-    document.getElementById("textContainer").style.opacity = 0;
-    document.getElementById("fadeOutContainer").style.opacity = 1;
-    document.getElementById("videoPlayer").style.opacity = 0;
-    document.getElementById("video").pause();
 
-    for (i = 0; i < scene.meshes.length; i++) {
-        scene.meshes[i].dispose()
-    }
 
-    BABYLON.SceneLoader.ImportMesh("", "./models/", meshName, scene, function(scene) {
-        document.getElementById("fadeOutContainer").style.opacity = 0;
+const canvasTwo = document.getElementById("secondCanvas")
+const engineTwo = new BABYLON.Engine(canvasTwo, true);
+
+function createSceneTwo() {
+
+    const sceneTwo = new BABYLON.Scene(engineTwo);
+    sceneTwo.clearColor = new BABYLON.Color3(0.73, 0.89, 0.96);
+
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 2, new BABYLON.Vector3(0, 0, 0), sceneTwo);
+    camera.useAutoRotationBehavior = true;
+    camera.autoRotationBehavior.idleRotationSpeed = 0.7;
+    camera.minZ = 0.1;
+    camera.lowerRadiusLimit = camera.upperRadiusLimit = camera.radius;
+    camera.attachControl(canvasTwo, true);
+    
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), sceneTwo);
+    
+    var gl = new BABYLON.GlowLayer("glow", sceneTwo);
+    gl.intensity = 1.5;
+
+    BABYLON.SceneLoader.ImportMesh("", "./models/", "petrol.glb", sceneTwo, function(sceneTwo) {
         setTimeout(function() {
-            // document.getElementById("fadeOutContainer").remove();
         }, 1000)
     })
 
-}
-
-
-function loadVideo() {
-
-    for (i = 0; i < scene.meshes.length; i++) {
-        scene.meshes[i].dispose()
+    if (window.matchMedia("(max-width: 700px)").matches) {
+        camera.detachControl()
     }
 
-    document.getElementById("textContainer").style.opacity = 0;
-    document.getElementById("videoPlayer").style.opacity = 1;
-    document.getElementById("video").play();
-}
-
-changeMesh("Gen.glb")
-
-
-var modelData = {
-    "Gen": ["133,824", "133,706", "267,412"],
-    "Fan": ["8,169", "8,944", "8,944"],
-    "Petrol": ["52,237", "51,500", "51,500"]
-}
-
-function loadData(model) {
-    var verts = document.getElementById("verts")
-    var faces = document.getElementById("faces")
-    var tris = document.getElementById("tris")
-    
-    let data = modelData[model];
-    
-    verts.innerHTML = data[0];
-    faces.innerHTML = data[1];
-    tris.innerHTML = data[2];
-    
-    document.getElementById("textContainer").style.opacity = 1;
+    return sceneTwo;
 
 }
+
+const sceneTwo = createSceneTwo();
+
+engineTwo.runRenderLoop(function () {
+    sceneTwo.render();
+});
+
+window.addEventListener("resize", function () {
+    engineTwo.resize();
+});
